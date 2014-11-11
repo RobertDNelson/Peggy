86385c0429cd38cd84a464bdb7b76fbe34f214bb
Robert Nelson <robert.nelson@stateless-software.com>
2014-11-11 08:38:38 -0600

    Skipping spaces in DiscoText.

--- Modules/discoText.js
+++ Modules/discoText.js
@@ -13,17 +13,23 @@ function update() {
 
   if (text && text.length > 0)
   {
-    var randomSlot = Math.floor(Math.random() * (text.length));
-    var randomChar = text.substring(randomSlot, randomSlot + 1);
+    var randomChar = " ";
+    var randomSlot = 0;
+
+    while (randomChar === " ") {
+      randomSlot = Math.floor(Math.random() * (text.length));
+      randomChar = text.substring(randomSlot, randomSlot + 1);
+    }
+
     var randomColor = colors[Math.floor(Math.random() * (3))];
 
-	var options = {
+    var options = {
       host: '10.105.4.251',
       path: '/peggy/write?board=1&x=' + (randomSlot + 5) + '&y=4&text=' + encodeURIComponent(randomColor + randomChar),
       agent: false
     };
 
-	http.get(options, function(res) {
+    http.get(options, function(res) {
       }).on('error', function(e) {
         console.log("Got error: " + e.message);
       });
