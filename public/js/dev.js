3b737aaa71fd3eb31b8d810eaab1107abd195862
Nate Chrysler <nchrysler@rightsourcecompliance.com>
2018-09-02 20:28:27 -0500

    added a board printer form to the Dev Console page
    useful for drawing ASCII art locally

--- public/js/dev.js
+++ public/js/dev.js
@@ -17,8 +17,52 @@
 		}
 	}
 
+	function printBoard() {
+		var text = $("#art").val();
+		var board = parseInt($("#sendto").val());
+		window.writeBoard(board, text);
+	}
+
+	function clearBoard() {
+		var board = parseInt($("#sendto").val());
+		window.clearBoard(board);
+	}
+
+	window.write = function(board, x, y, text) {
+		$.ajax({
+			type: "GET",
+			url: "/peggy/write?board=" + encodeURIComponent(board) + "&x=" + encodeURIComponent(x) + "&y=" + encodeURIComponent(y) + "&text=" + encodeURIComponent(text),
+			dataType: "html",
+			success: function(data, textStatus, jqXHR) {
+				console.log("Write completed successfully.");
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log("Write failed (" + errorThrown + "). Details: ", jqXHR);
+			}
+		})
+	};
+
+	window.writeBoard = function(board, text) {
+		var lines = text.split("\n");
+		console.log(lines);
+		for (var i=0; i<lines.length; i++) {
+			if (i > 11) break;
+			var line = lines[i] + "                                                                                ";
+			window.write(board, 0, i, line);
+		}
+	};
+
+	window.clearBoard = function(board) {
+		for (var i=0; i<12; i++) {
+			var line = "                                                                                ";
+			window.write(board, 0, i, line);
+		}
+	};
+
 	$(document).ready(function() {
+		$("#send").click(printBoard);
+		$("#clear").click(clearBoard);
 		loadAllBoards();
-		setInterval(loadAllBoards, 1000);
+		setInterval(loadAllBoards, 500);
 	});
 })(jQuery);
